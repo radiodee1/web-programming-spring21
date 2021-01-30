2881e8f8ad9ff2e8f87a99a67f0bb331ccb018ec
David Liebman <radiodee1@gmail.com>
2021-01-30 11:56:30 -0500

    insert some rows.

---
+++ sql/demo_create_rows.js
@@ -0,0 +1,35 @@
+const sql = require("./populate.js");
+
+//insert
+likes = {
+    name:"likes",
+    columns_list: ['post_id', 'from_user_id'],
+    mult_rows: true,
+    values_list: [
+        [1111, 2222],
+        [3333, 4444],
+        [8888, 9999]
+    ]
+};
+
+//select
+post_ids = {
+    name: "likes",
+    columns_list: ['*'],
+    mult_rows: false
+}
+
+
+x = sql.makeInsert(likes.name, likes.columns_list, likes.values_list, likes.mult_rows);
+
+console.log(x);
+
+con = sql.connection();
+
+//sql.query(con, x);
+
+x = sql.makeSelect(post_ids.name, post_ids.columns_list, "", post_ids.mult_rows);
+
+sql.query(con, x);
+
+sql.end(con);
