e5494ffacd02ff9f6e61127d3e3d46e32e8767f2
David Liebman <radiodee1@gmail.com>
2021-01-30 08:34:20 -0500

    password as first parameter

--- sh/docker_mysql.sh
+++ sh/docker_mysql.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
+
+echo $# $1
+
+if [[ "$#" != "1" ]] ; then
+    echo "one parameter needed!"
+    echo "password for container"
+    exit
+fi
+
+echo "starting docker server."
+
+
 docker pull mysql/mysql-server:5.6.42
-docker run --name=mysql1 -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=droptest1 -d mysql/mysql-server:5.6.42 
+docker run --name=mysql1 -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD="$1" -d mysql/mysql-server:5.6.42 
 docker start mysql1 
 echo
 ip a | grep docker | grep inet
