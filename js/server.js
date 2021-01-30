108e3e9789680f3c6ae2941b02c2e6b2f90f4192
David Liebman <radiodee1@gmail.com>
2021-01-30 17:26:17 -0500

    add express

--- js/server.js
+++ js/server.js
@@ -1,14 +1,13 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+  res.json({message:"Welcome."});
+})
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
