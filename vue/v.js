e1c7b0c72cf93d8df1d0476e743706342ef0862c
David Liebman <radiodee1@gmail.com>
2021-01-30 17:27:22 -0500

    add vue test

---
+++ vue/v.js
@@ -0,0 +1,16 @@
+function doLoad() {
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello World!'
+    }
+  })
+}
+  
+$(document).ready(function() {
+    /* new app(); */
+    doLoad();
+})
+    
+    //alert('hello');
