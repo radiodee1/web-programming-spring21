2cbbba6b89d88ad6f1b53db0ae03caf29fb27ed3
David Liebman <radiodee1@gmail.com>
2021-02-15 07:34:52 -0500

    refactor

---
+++ webpack-vue/src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- start home -->
+  <section class="section gray" v-if="home">
+    <div class="buttons">
+      <a class="button is-primary" onclick="focusRegister();">
+        <strong>Sign up</strong>
+      </a>
+      <a class="button is-light" onclick="focusLogin()"> Log in </a>
+    </div>
+    Login or register!
+  </section>
+  <!-- end home -->
+</template>
+
+<script>
+require("../js/v.js");
+require("../js/populate.js");
+require("../js/controls.js");
+
+import { doLoad, visibility, classOption, focusRegister } from "../js/v.js";
+
+export default {
+  name: "home",
+  data() {
+    return {};
+  },
+  props: {
+    home: Boolean,
+  },
+  methods: {
+    classOption: function (i) {
+      //console.log(i);
+      const x = Boolean(i);
+      if (x === true) return "visi";
+      else return "invis";
+    },
+  },
+};
+</script>
---
+++ webpack-vue/src/components/Template.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>Navigation Menu</div>
+</template>
+
+<script>
+require("../js/v.js");
+require("../js/populate.js");
+require("../js/controls.js");
+
+
+export default {
+  name: "navigation",
+  data() {
+    return {};
+  },
+  props: {
+    newsfeed: Boolean,
+    //classOption: Function
+  },
+  methods: {
+    classOption: function (i) {
+      //console.log(i);
+      const x = Boolean(i);
+      if (x === true) return "visi";
+      else return "invis";
+    },
+  },
+};
+</script>
