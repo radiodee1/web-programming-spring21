9b4c2248d46266bc4932e43bf3112d49b0057fd7
David Liebman <radiodee1@gmail.com>
2021-02-16 09:33:51 -0500

    start feed item

---
+++ webpack-vue/src/components/FeedItem.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="" class="card"  v-show="visible" ref="" >
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img src="./pic/app.png" alt="Placeholder image" id="">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+          <figure class="image is-48x48">
+            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="media-content">
+          <p class="title is-4"> {{ message_obj_from }} </p>
+          <!-- p class="subtitle is-6">@johnsmith</p -->
+        </div>
+      </div>
+      <!-- three contents -->
+      <!--  {{ visible }} -->
+      <div class="content"  v-bind:class=" classExercise(show_exercise)">
+        <pre>{{exercise_obj_message}}</pre>
+        <a>@bulmaio</a>.
+        <a href="#">#css</a> <a href="#">#responsive</a>
+        <br>
+        <time datetime="">{{ date_now }}</time>
+      </div>
+
+      <div class="content"  v-bind:class=" classMessage(show_message)">
+        message - {{ message_obj_message }} <!-- a>@bulmaio</a>.
+        <a href="#">#css</a> <a href="#">#responsive</a -->
+        <br>
+        <time datetime="">{{ date_now }}</time>
+      </div>
+
+      <div class="content"  v-bind:class=" classWorkout(show_workout)">
+        workout - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+        <a href="#">#css</a> <a href="#">#responsive</a>
+        <br>
+        <time datetime="">{{ date_now }}</time>
+      </div>
+      <!-- end three contents -->
+    </div>
+  </div>
+</template>
+
+<script>
+//require("../js/v.js");
+//require("../js/populate.js");
+//require("../js/controls.js");
+
+
+export default {
+  name: "item",
+  data() {
+    return {};
+  },
+  props: {
+    newsfeed: Boolean,
+    //classOption: Function
+  },
+  methods: {
+    classOption: function (i) {
+      //console.log(i);
+      const x = Boolean(i);
+      if (x === true) return "visi";
+      else return "invis";
+    },
+  },
+};
+</script>
