f8acb29087d6e9131c5d12dfd51e284b6a85c219
David Liebman <radiodee1@gmail.com>
2021-02-18 12:14:12 -0500

    Friends.vue improvements

--- webpack-vue/src/components/Friends.vue
+++ webpack-vue/src/components/Friends.vue
@@ -13,19 +13,25 @@
             ></button>
           </div>
           <div class="message-body gray">
-            
-            <table>
+            <table class="table is-striped is-narrow is-hoverable is-fullwidth">
               <tr>
-                <th> Name </th>
-                <th> Username </th>
-                <th> Status </th>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Status</th>
+                <th>Controls</th>
               </tr>
               <tr v-for="l in list" :key="l" class="list-style">
-                <td class=""> {{l.firstname}} {{l.lastname}} </td>
-                <td class=""> {{l.username}} </td>
-                <td class=""> {{l.status}} </td>
-              </tr>
+                <td class="">{{ l.firstname }} {{ l.lastname }}</td>
+                <td class="">{{ l.username }}</td>
+                <td class="">{{ l.status }}</td>
+                <td>
+                  <button class="button is-primary is-small" v-if="l.status =='new'">Ask</button>
+                  <button class="button is-primary is-small" v-if="l.status =='asked'">Approve</button>
+                  <span v-if="l.status == 'confirmed'">Confirmed</span>
+                  <span v-if="l.status == 'waiting'">Waiting</span>
 
+                </td>
+              </tr>
             </table>
             <div class="control">
               <nav class="level">
@@ -52,7 +58,7 @@ export default {
   name: "friends",
   data() {
     return {
-      list: this.makeList()
+      list: this.makeList(),
     };
   },
   props: {
@@ -74,18 +80,20 @@ export default {
         firstname: "David",
         lastname: "Liebman",
         username: "xliebman",
-        status: "confirmed" , // confirmed, asked, new, etc.
+        status: "waiting", // confirmed, asked, new, waiting,  etc.
       };
       l.push(dict);
       dict.firstname = "Dave";
       l.push(dict);
       return l;
-    }
+    },
   },
 };
 </script>
 <style scoped>
-.list-style, td, th {
+.list-style,
+td,
+th {
   border: 3px solid black;
   border-collapse: collapse;
   border-left: 3px solid black;
