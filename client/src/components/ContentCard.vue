2c19ab0d0d6faf58f701df020b5c3b366a0ca335
David Liebman <radiodee1@gmail.com>
2021-03-04 13:22:49 -0500

    update to class version

--- client/src/components/ContentCard.vue
+++ client/src/components/ContentCard.vue
@@ -1,46 +1,43 @@
 <template>
   <div class="card">
-      <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img
-              :src="post.user.pic"
-              :alt="post.alt"
-            />
-          </figure>
-        </div>
-        <div class="media-content">
-          <p class="title is-4">{{post.user.name}} </p>
-          <p class="subtitle is-6">{{post.user.handle}}</p>
-        </div>
+      <div class="card-content">
+        <div class="media">
+            <div class="media-left">
+                <figure class="image is-48x48">
+                    <img :src="post.user.pic" :alt="post.user.name" />
+                </figure>
+            </div>
+            <div class="media-content">
+                <p class="title is-4">{{post.user.name}}</p>
+                <p class="subtitle is-6">{{post.user.handle}}</p>
+            </div>
+      </div>
+
       </div>
     <div class="card-image">
       <figure class="image is-4by3">
-        <img
-          :src="post.src"
-          :alt="post.alt"
+        <img :src="post.src" :alt="post.alt"
         />
       </figure>
     </div>
     <div class="card-content">
-      
 
       <div class="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-        iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
-        <a href="#">#responsive</a>
+        {{post.caption}}
         <br />
-        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        <time :datetime="post.time">{{post.time}}</time>
       </div>
     </div>
   </div>
 </template>
+
 <script>
 export default {
-    props: {post: Object}
-}
+    props: {
+        post: Object
+    }
+};
 </script>
 
-<style scoped>
-
+<style>
 </style>
